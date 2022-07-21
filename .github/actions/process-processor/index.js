123831ce1199a4def71d81dd6d53a2dd8aef5892
Anuj Deshpande <anuj@makerville.io>
2022-07-21 10:48:19 +0530

    Add reusable github action

--- .github/actions/process-processor/index.js
+++ .github/actions/process-processor/index.js
@@ -1,8 +1,24 @@
 const core = require("@actions/core");
+const style = require("ansi-styles");
+const github = require('@actions/github');
+
+const yellow = (v) => `${style.yellow.open}${v}${style.yellow.close}`;
+const green = (v) => `${style.green.open}${v}${style.green.close}`;
 
 async function run(){
-    core.setOutput("Hello world");
-    console.log("Lorem ipsum");
+    core.info(green("Process Processor"));    
+
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+
+    const repoName = JSON.stringify(github.context.payload.repository.name);
+    const orgName = JSON.stringify(github.context.payload.repository.organization);
+    const authorName = JSON.stringify(github.context.payload.pusher.name);
+    
+    core.info(yellow(["Org name  ", orgName].join("")))
+    core.info(yellow(["Repo name ", repoName].join("")))
+    core.info(yellow(["Author    ", authorName].join("")))
+
+    core.info(github.context.payload)
 }
 
 run();
