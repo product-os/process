1b9a7d458e6a17f126e83d2c1dd99033c498d9fa
Anuj Deshpande <anuj@makerville.io>
2022-07-28 14:09:47 +0530

    Add yaml titles

--- .github/actions/process-processor/index.js
+++ .github/actions/process-processor/index.js
@@ -23,37 +23,37 @@ async function generateMermaid(file){
     //    console.log('graph');
     mermaid = mermaid.concat('graph\n');
     for (const element of elements) {
-        if (element.type === 'transformer') {
-            tarray[tindex++] = element.handle;
-        }
-        else if (element.type === 'input-output') {
+	if (element.type === 'transformer') {
+	    tarray[tindex++] = element.handle;
+	}
+	else if (element.type === 'input-output') {
 
-            // console.log("IO" + ioindex.toString() + "[<b>" + element.handle + "</b>]");
-            mermaid = mermaid.concat("IO" + ioindex.toString() + "[/<b>" + element.handle + "</b>");
+	    // console.log("IO" + ioindex.toString() + "[<b>" + element.handle + "</b>]");
+	    mermaid = mermaid.concat("IO" + ioindex.toString() + "[/<b>" + element.handle + "</b>");
 
-            for (let req in element.required) {
-                // console.log("<br/>-", element.required[req]);
-                mermaid = mermaid.concat("<br/>-", element.required[req]);
-            }
-            for (let opt in element.optional) {
-                // console.log("<br/>- optional ", element.optional[opt]);
-                mermaid = mermaid.concat("<br/>- optional ", element.optional[opt]);
-            }
-            ioarray[ioindex++] = element.handle;
-            mermaid = mermaid.concat('/]\n');
-        }
+	    for (let req in element.required) {
+		// console.log("<br/>-", element.required[req]);
+		mermaid = mermaid.concat("<br/>-", element.required[req]);
+	    }
+	    for (let opt in element.optional) {
+		// console.log("<br/>- optional ", element.optional[opt]);
+		mermaid = mermaid.concat("<br/>- optional ", element.optional[opt]);
+	    }
+	    ioarray[ioindex++] = element.handle;
+	    mermaid = mermaid.concat('/]\n');
+	}
     }
     for (const element of elements) {
-        if (element.type === 'transformer') {
-            // The next 2 lines mean that we can only use one input or output for each transformer
-            // if there are more than 1 mentioned, they will be ignored
-            var inputindex = ioarray.indexOf(element.input[0]);
-            var outputindex = ioarray.indexOf(element.output[0]);
-            var tindex = tarray.indexOf(element.handle);
-            // console.log('IO' + inputindex + ' --> T' + tindex + '{' + element.handle + '} --> IO' + outputindex);
-            mermaid = mermaid.concat('IO' + inputindex + ' --> T' + tindex + '[' + element.handle + '] --> IO' + outputindex)
-            mermaid = mermaid.concat('\n');
-        }
+	if (element.type === 'transformer') {
+	    // The next 2 lines mean that we can only use one input or output for each transformer
+	    // if there are more than 1 mentioned, they will be ignored
+	    var inputindex = ioarray.indexOf(element.input[0]);
+	    var outputindex = ioarray.indexOf(element.output[0]);
+	    var tindex = tarray.indexOf(element.handle);
+	    // console.log('IO' + inputindex + ' --> T' + tindex + '{' + element.handle + '} --> IO' + outputindex);
+	    mermaid = mermaid.concat('IO' + inputindex + ' --> T' + tindex + '[' + element.handle + '] --> IO' + outputindex)
+	    mermaid = mermaid.concat('\n');
+	}
     }
     mermaid = mermaid.concat('\n```\n')
     // fs.writeFileSync('process-diagram.md', mermaid, { encoding: 'utf8' })
@@ -70,26 +70,35 @@ async function parseDocs(filePath) {
 	.parse(fileContents);
     let myYAML = '';
     // console.log(JSON.stringify(myResult, null, 2))
-    
+
     for (let i=0; i< myResult.children.length; i++){
-	
+
 	switch (myResult.children[i].type){
 	case 'heading':
 	    // console.log(myResult.children[i].children[0].value)
 	    fs.appendFileSync(filePath.split('.')[0]+'.yml',myResult.children[i].children[0].value+'\n');
-	    break;
-	case 'paragraph':
-	    // dont print the paragraph
+	    if (myResult.children[i].children[0].value === 'Title') {
+		fs.appendFileSync(filePath.split('.')[0]+'.yml',"name: "+myResult.children[i].children[0].value+'\n');
+		fs.appendFileSync(filePath.split('.')[0]+'.yml',"type: transformer"+'\n');
+	    }
+	    else { // input output type
+		fs.appendFileSync(filePath.split('.')[0]+'.yml',"name: "+myResult.children[i].children[0].value+'\n');
+		fs.appendFileSync(filePath.split('.')[0]+'.yml',"type: input-output"+'\n');
+	    }
 	    break;
 	case 'list':
+	    fs.appendFileSync(filePath.split('.')[0]+'.yml', "required: "+'\n');
 	    for (let j=0; j<myResult.children[i].children.length; j++){
 		// console.log("  - ", myResult.children[i].children[j].children[0].children[0].value)
 		fs.appendFileSync(filePath.split('.')[0]+'.yml', "  - "+myResult.children[i].children[j].children[0].children[0].value+'\n');
 	    }
 	    break;
-	    
+	case 'paragraph':
+	    // dont print the paragraph
+	    break;
+
 	}
-	
+
     }
     // print the contents of the yaml file
     const yamlContents = fs.readFileSync(filePath.split('.')[0]+'.yml').toString()
@@ -98,18 +107,18 @@ async function parseDocs(filePath) {
 }
 
 async function run(){
-    core.info(green("Process Processor"));    
+    core.info(green("Process Processor"));
 
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
 
     const repoName = JSON.stringify(github.context.payload.repository.name);
     const orgName = JSON.stringify(github.context.payload.repository.organization);
     const authorName = JSON.stringify(github.context.payload.pusher.name);
-    
+
     core.info(yellow(["Org name  ", orgName].join("")))
     core.info(yellow(["Repo name ", repoName].join("")))
     core.info(yellow(["Author    ", authorName].join("")))
-    
+
     // core.info(JSON.stringify(github.context.payload, null, 2))
 
     // Look for files only in the docs folder
